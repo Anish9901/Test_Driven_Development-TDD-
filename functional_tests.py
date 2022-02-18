e995239991344b3345ca882a685c260d9536b042
Anish <umaleanish120@gmail.com>
2022-02-18 22:46:32 +0530

    Extending functional_tests using python's unittest module

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,15 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+    def tearDown(self):
+        self.browser.quit()
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do',self.browser.title)
+        self.fail('Finish the test!')
 
-assert 'Django' in browser.title
+if __name__ == '__main__':
+    unittest.main()
