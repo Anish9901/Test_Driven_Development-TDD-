812487d80d8acd294703c5871e6a8ff37b913b23
Anish <umaleanish120@gmail.com>
2022-02-20 15:26:51 +0530

    Added check for rendering the correct html page

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,7 @@ from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
+from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
@@ -13,6 +14,10 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>',html)
-        self.assertTrue(html.endswith('</html>'))
+        expected_html = render_to_string('home.html')
+        self.assertEqual(html,expected_html)
+
+    #Instead of self.assertEqual(html,expected_html) we can also use
+    def test_home_page_returns_correct_html_using_Django_test_client(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response,'home.html')
